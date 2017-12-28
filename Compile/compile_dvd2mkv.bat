2734ebba47de97ae604c6e46ba0c55cfa7e5a67e
ech0wav3 <ech0wav3@gmail.com>
2017-12-28 14:43:02 -0500

    settings and install files update

--- Compile/compile_dvd2mkv.bat
+++ Compile/compile_dvd2mkv.bat
@@ -1,3 +1,13 @@
-bamcompile1.21\bamcompile -c ..\code\dvd2mkv.php ..\..\..\debug\curver\dvd2mkv.exe
-copy /y ..\code\settings.conf ..\..\..\debug\curver\settings.conf
+@echo off
+SET installdir=%CD:~0,3%
+
+mkdir %installdir%dvd2mkv10\
+echo Compiling dvd2mkv...
+bamcompile1.21\bamcompile -c ..\dvd2mkv.php %installdir%dvd2mkv10\dvd2mkv.exe
+echo Copying settings...
+copy /y ..\settings.conf %installdir%dvd2mkv10\settings.conf
+echo Copying required files...
+robocopy "..\Support Apps " %installdir%dvd2mkv10\ /s /e
+echo Setup complete!
+echo dvd2mkv can be found at: %installdir%dvd2mkv10\
 pause
